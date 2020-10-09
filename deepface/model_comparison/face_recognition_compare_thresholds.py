48900ea415f99f2895abcecd7f3ff73685734440
YCAyca <aktas.yagmur@gmail.com>
2020-10-09 22:30:06 +0200

    Update face_recognition_compare_thresholds.py

--- deepface/model_comparison/face_recognition_compare_thresholds.py
+++ deepface/model_comparison/face_recognition_compare_thresholds.py
@@ -6,7 +6,7 @@ Created on Thu Sep 24 20:28:45 2020
 """
 
 from deepface import DeepFace
-from deepface.basemodels import VGGFace, OpenFace, Facenet, FbDeepFace, DeepID
+from deepface.basemodels import VGGFace,Facenet
 from deepface.commons import functions, realtime, distance as dst
 
 import time
@@ -14,16 +14,14 @@ import os
 
 
 base_path = 'FaceRecognition_dataset3\\'
-osym_path = 'osym\\'
+anchor_path = 'anchor\\'
 
-#models = ["VGG-Face", "Facenet", "OpenFace", "DeepFace"]
-model_name= "Facenet"  # "Facenet"
-distance_metric = "euclidean_l2" # "euclidean_l2"
+model_name= "Facenet"  # "VGG-Fce"
+distance_metric = "euclidean_l2" 
 threshold = 1.05
-detector = "ssd" #opencv
+detector = "ssd" 
 
 log_file =  open("model_performation_logs_threshold.txt", "w+")
-
 
 if model_name == 'VGG-Face':
     log_file.write("------ Using VGG-Face model ------ \n\n\n")
@@ -48,18 +46,18 @@ log_file.write("Accuracy Calculation for Positive Samples \n")
 true_count = 0
 total_sample_count = 0
 no_face_count = 0
-for imageName in os.listdir(base_path + osym_path):
-    input_folder = base_path + imageName.split('_osym')[0]
+for imageName in os.listdir(base_path + anchor_path):
+    input_folder = base_path + imageName.split('_anchor')[0]
     for sampleImage in os.listdir(input_folder+'\\'):
         print(sampleImage)
         input_foto_path = input_folder +'\\' + sampleImage
-        osym_foto_path = base_path + osym_path + imageName
+        anchor_foto_path = base_path + anchor_path + imageName
         #----------------------
 
-        img1, face_flag = functions.preprocess_face(img=osym_foto_path, target_size=(input_shape_y, input_shape_x), enforce_detection = False, detector_backend = detector)
+        img1, face_flag = functions.preprocess_face(img=anchor_foto_path, target_size=(input_shape_y, input_shape_x), enforce_detection = False, detector_backend = detector)
         if face_flag == False:
-            print("no face detected on" + osym_foto_path + "\n")
-            log_file.write("no face detected on" + osym_foto_path + "\n")
+            print("no face detected on" + anchor_foto_path + "\n")
+            log_file.write("no face detected on" + anchor_foto_path + "\n")
             no_face_count += 1
             continue
         img2,face_flag = functions.preprocess_face(img=input_foto_path, target_size=(input_shape_y, input_shape_x), enforce_detection = False, detector_backend = detector)
@@ -99,18 +97,18 @@ total_sample_count = 0
 no_face_count = 0
 log_file.write("Using " + detector + " backend \n")
 
-for imageName in os.listdir(base_path + osym_path):
-    input_folder = base_path + "not_" + imageName.split('_osym')[0]
+for imageName in os.listdir(base_path + anchor_path):
+    input_folder = base_path + "not_" + imageName.split('_anchor')[0]
     for sampleImage in os.listdir(input_folder+'\\'):
         print(sampleImage)
         input_foto_path = input_folder +'\\' + sampleImage
-        osym_foto_path = base_path + osym_path + imageName
+        anchor_foto_path = base_path + anchor_path + imageName
         #----------------------
 
-        img1,face_flag = functions.preprocess_face(img=osym_foto_path, target_size=(input_shape_y, input_shape_x), enforce_detection = False, detector_backend = detector)
+        img1,face_flag = functions.preprocess_face(img=anchor_foto_path, target_size=(input_shape_y, input_shape_x), enforce_detection = False, detector_backend = detector)
         if face_flag == False:
-            print("no face detected on" + osym_foto_path + "\n")
-            log_file.write("no face detected on" + osym_foto_path + "\n")
+            print("no face detected on" + anchor_foto_path + "\n")
+            log_file.write("no face detected on" + anchor_foto_path + "\n")
             no_face_count += 1
             continue
         img2,face_flag = functions.preprocess_face(img=input_foto_path, target_size=(input_shape_y, input_shape_x), enforce_detection = False, detector_backend = detector)
